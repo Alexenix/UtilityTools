cc201e3be1b72f386ee9b80cf794339813334b9b
Alexenix <alexenix@hotmail.com>
2015-07-04 11:35:45 +0800

    Add Doc Generator
    Add Doc Generator

---
+++ DocumentGenerator/DocumentGenerator/Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace DocumentGenerator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxCSVInput = new System.Windows.Forms.TextBox();
+            this.textBoxDocTemplate = new System.Windows.Forms.TextBox();
+            this.textBoxTargetFolder = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.buttonCSV = new System.Windows.Forms.Button();
+            this.buttonDocTemplate = new System.Windows.Forms.Button();
+            this.buttonTargetFolder = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.toolStripProgressBar1 = new System.Windows.Forms.ToolStripProgressBar();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 62);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(129, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "文档模板 (Doc/Docx)： ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 24);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(162, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "投资人列表文件 (CSV/TXT)： ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 100);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "目标文件夹： ";
+            // 
+            // textBoxCSVInput
+            // 
+            this.textBoxCSVInput.Location = new System.Drawing.Point(171, 22);
+            this.textBoxCSVInput.Name = "textBoxCSVInput";
+            this.textBoxCSVInput.ReadOnly = true;
+            this.textBoxCSVInput.Size = new System.Drawing.Size(389, 20);
+            this.textBoxCSVInput.TabIndex = 3;
+            // 
+            // textBoxDocTemplate
+            // 
+            this.textBoxDocTemplate.Location = new System.Drawing.Point(171, 59);
+            this.textBoxDocTemplate.Name = "textBoxDocTemplate";
+            this.textBoxDocTemplate.ReadOnly = true;
+            this.textBoxDocTemplate.Size = new System.Drawing.Size(389, 20);
+            this.textBoxDocTemplate.TabIndex = 4;
+            // 
+            // textBoxTargetFolder
+            // 
+            this.textBoxTargetFolder.Location = new System.Drawing.Point(171, 93);
+            this.textBoxTargetFolder.Name = "textBoxTargetFolder";
+            this.textBoxTargetFolder.ReadOnly = true;
+            this.textBoxTargetFolder.Size = new System.Drawing.Size(389, 20);
+            this.textBoxTargetFolder.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(160, 139);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(336, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "确认生成";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // buttonCSV
+            // 
+            this.buttonCSV.Location = new System.Drawing.Point(568, 19);
+            this.buttonCSV.Name = "buttonCSV";
+            this.buttonCSV.Size = new System.Drawing.Size(87, 23);
+            this.buttonCSV.TabIndex = 8;
+            this.buttonCSV.Text = "选择文件...";
+            this.buttonCSV.UseVisualStyleBackColor = true;
+            this.buttonCSV.Click += new System.EventHandler(this.buttonCSV_Click);
+            // 
+            // buttonDocTemplate
+            // 
+            this.buttonDocTemplate.Location = new System.Drawing.Point(568, 58);
+            this.buttonDocTemplate.Name = "buttonDocTemplate";
+            this.buttonDocTemplate.Size = new System.Drawing.Size(87, 23);
+            this.buttonDocTemplate.TabIndex = 9;
+            this.buttonDocTemplate.Text = "选择文件...";
+            this.buttonDocTemplate.UseVisualStyleBackColor = true;
+            this.buttonDocTemplate.Click += new System.EventHandler(this.buttonDocTemplate_Click);
+            // 
+            // buttonTargetFolder
+            // 
+            this.buttonTargetFolder.Location = new System.Drawing.Point(568, 90);
+            this.buttonTargetFolder.Name = "buttonTargetFolder";
+            this.buttonTargetFolder.Size = new System.Drawing.Size(87, 23);
+            this.buttonTargetFolder.TabIndex = 10;
+            this.buttonTargetFolder.Text = "选择文件夹...";
+            this.buttonTargetFolder.UseVisualStyleBackColor = true;
+            this.buttonTargetFolder.Click += new System.EventHandler(this.buttonTargetFolder_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel,
+            this.toolStripProgressBar1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 204);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(681, 22);
+            this.statusStrip1.TabIndex = 11;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel
+            // 
+            this.toolStripStatusLabel.Name = "toolStripStatusLabel";
+            this.toolStripStatusLabel.Size = new System.Drawing.Size(180, 17);
+            this.toolStripStatusLabel.Text = "欢迎使用Word文档批量生成工具";
+            // 
+            // folderBrowserDialog1
+            // 
+            this.folderBrowserDialog1.Description = "请选择生成文档的目标文件夹";
+            // 
+            // toolStripProgressBar1
+            // 
+            this.toolStripProgressBar1.Name = "toolStripProgressBar1";
+            this.toolStripProgressBar1.Size = new System.Drawing.Size(100, 16);
+            this.toolStripProgressBar1.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(681, 226);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.buttonTargetFolder);
+            this.Controls.Add(this.buttonDocTemplate);
+            this.Controls.Add(this.buttonCSV);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBoxTargetFolder);
+            this.Controls.Add(this.textBoxDocTemplate);
+            this.Controls.Add(this.textBoxCSVInput);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Word文档批量生成工具";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxCSVInput;
+        private System.Windows.Forms.TextBox textBoxDocTemplate;
+        private System.Windows.Forms.TextBox textBoxTargetFolder;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button buttonCSV;
+        private System.Windows.Forms.Button buttonDocTemplate;
+        private System.Windows.Forms.Button buttonTargetFolder;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.ToolStripProgressBar toolStripProgressBar1;
+    }
+}
+
---
+++ DocumentGenerator/DocumentGenerator/Form1.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace DocumentGenerator
+{
+    public partial class Form1 : Form
+    {
+        private string csvInputFilePath = string.Empty;
+        private string docTemplateFilePath = string.Empty;
+        private string targetOutputFolder = string.Empty;
+        private object missing = System.Reflection.Missing.Value;
+        private const string EMPTY_PLACE_HOLDER = "          ";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonCSV_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Title = "请选择投资人列表文件";
+            openFileDialog1.FileName = string.Empty;
+            openFileDialog1.Filter = "CSV Files(*.csv)|*.csv|Text Files(*.txt)|*.txt";
+            openFileDialog1.FilterIndex = 1;
+            openFileDialog1.CheckFileExists = true;
+            openFileDialog1.CheckPathExists = true;
+            openFileDialog1.Multiselect = false;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBoxCSVInput.Text = openFileDialog1.FileName;
+                csvInputFilePath = openFileDialog1.FileName;
+                toolStripStatusLabel.Text = string.Format("投资人列表文件选择成功: {0}", csvInputFilePath);
+            }
+
+            openFileDialog1.Dispose();
+        }
+
+        private void buttonDocTemplate_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Title = "请选择Word文档模板";
+            openFileDialog1.FileName = string.Empty;
+            openFileDialog1.Filter = "Word Document(*.docx)|*.docx|Word 97-2003 Document(*.doc)|*.doc";
+            openFileDialog1.FilterIndex = 1;
+            openFileDialog1.CheckFileExists = true;
+            openFileDialog1.CheckPathExists = true;
+            openFileDialog1.Multiselect = false;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                //t .Text = openFileDialog1.FileName;
+                textBoxDocTemplate.Text = openFileDialog1.FileName;
+                docTemplateFilePath = openFileDialog1.FileName;
+                toolStripStatusLabel.Text = string.Format("Word文档模板选择成功: {0}", docTemplateFilePath);
+            }
+
+            openFileDialog1.Dispose();
+        }
+
+        private void buttonTargetFolder_Click(object sender, EventArgs e)
+        {
+            // Show the FolderBrowserDialog.
+            DialogResult result = folderBrowserDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                textBoxTargetFolder.Text = folderBrowserDialog1.SelectedPath;
+                targetOutputFolder = folderBrowserDialog1.SelectedPath;
+                toolStripStatusLabel.Text = string.Format("目标文件夹选择成功: {0}", targetOutputFolder);
+            }
+
+            folderBrowserDialog1.Dispose();
+        }
+       
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(csvInputFilePath) || string.IsNullOrEmpty(docTemplateFilePath) || string.IsNullOrEmpty(targetOutputFolder)) 
+            {
+                MessageBox.Show("请确保投资人列表文件、文档模板和目标文件夹都已经正确选择。");
+                return;
+            }
+
+            try
+            {
+                if (!File.Exists(csvInputFilePath))
+                {
+                    MessageBox.Show(string.Format("投资人列表文件不存在: {0}", csvInputFilePath));
+                    return;
+                }
+
+                if (!File.Exists(docTemplateFilePath))
+                {
+                    MessageBox.Show(string.Format("文档模板文件不存在: {0}", docTemplateFilePath));
+                    return;
+                }
+
+                if (!Directory.Exists(targetOutputFolder))
+                {
+                    Directory.CreateDirectory(targetOutputFolder);
+                }
+
+                string[] allLines = null;
+                allLines = File.ReadAllLines(csvInputFilePath);
+                if (allLines == null || allLines.Length == 0) 
+                {
+                    MessageBox.Show(string.Format("投资人列表文件为空: {0}", csvInputFilePath));
+                    return;
+                }
+
+                toolStripStatusLabel.Text = "文档批量生成中，请等待...";
+                DisableButtons();
+                toolStripProgressBar1.Maximum = allLines.Length - 1;
+                toolStripProgressBar1.Minimum = 0;
+                toolStripProgressBar1.Step = 1;
+                toolStripProgressBar1.Value = 0;
+
+                //construct column keys
+                IDictionary<string, string> columnKeyValuePairs = new Dictionary<string, string>();
+                string[] keyArray = allLines[0].Split(new char[] { '|' },StringSplitOptions.None);
+                for (int i = 0; i < keyArray.Length; i++) 
+                {
+                    columnKeyValuePairs.Add(new KeyValuePair<string, string>(keyArray[i], string.Empty));
+                }
+                
+                string currentLine = string.Empty;
+                string tempOutputFilePath = string.Empty;
+                string tempOutputFileName = string.Empty;
+                string outputFileExtension = docTemplateFilePath.EndsWith("docx") ? ".docx" : ".doc";                
+                Microsoft.Office.Interop.Word.Application wordApp = new Microsoft.Office.Interop.Word.Application { Visible = false };
+
+                for (int i = 1; i < allLines.Length; i++) 
+                {
+                    currentLine = allLines[i];
+                    string[] valueArray = currentLine.Split(new char[] { '|' }, StringSplitOptions.None);
+                    if (valueArray.Length != keyArray.Length) 
+                    {
+                        //Todo: Add error prompt
+                        toolStripProgressBar1.PerformStep();
+                        continue;
+                    }
+
+                    bool containsEmptyField = false;
+                    for (int k = 0; k < keyArray.Length; k++)
+                    {
+                        if (string.IsNullOrEmpty(valueArray[k]))
+                        {
+                            columnKeyValuePairs[keyArray[k]] = EMPTY_PLACE_HOLDER;
+                            containsEmptyField = true;
+                        }
+                        else
+                        {
+                            columnKeyValuePairs[keyArray[k]] = valueArray[k];
+                        }
+                    }
+
+                    if (containsEmptyField == true)
+                    {
+                        tempOutputFileName = string.Format("{0}-{1}-{2}-Incomplete{3}", i, valueArray[0], valueArray[1], outputFileExtension);
+                    }
+                    else 
+                    {
+                        tempOutputFileName = string.Format("{0}-{1}-{2}{3}", i, valueArray[0], valueArray[1], outputFileExtension);                        
+                    }
+
+                    tempOutputFilePath = Path.Combine(targetOutputFolder, tempOutputFileName);
+                    File.Copy(docTemplateFilePath, tempOutputFilePath, true);
+
+                    ReplaceDocumentContent(wordApp, tempOutputFilePath, columnKeyValuePairs);
+                    toolStripProgressBar1.PerformStep();               
+                }
+
+                wordApp.Quit(ref missing, ref missing, ref missing);
+                toolStripStatusLabel.Text = string.Format("批量生成文档成功, 目标文件夹: {0}", targetOutputFolder);
+
+            }
+            catch (Exception exp)
+            {
+                MessageBox.Show(string.Format("Exception Happened: {0}", exp.ToString()));
+            }
+            finally 
+            {
+                EnableButtons();
+            }
+        }
+
+        private void ReplaceDocumentContent(Microsoft.Office.Interop.Word.Application wordApp, object outputFilePath, IDictionary<string, string> columnKeyValuePairs) 
+        {
+            Microsoft.Office.Interop.Word.Document docObj = wordApp.Documents.Open(ref outputFilePath, ReadOnly: false, Visible: true);
+            docObj.Activate();
+
+            Microsoft.Office.Interop.Word.Find fnd = wordApp.ActiveWindow.Selection.Find;
+
+            foreach (KeyValuePair<string, string> pair in columnKeyValuePairs)
+            {
+                fnd.ClearFormatting();
+                fnd.Replacement.ClearFormatting();
+                fnd.Forward = true;
+                fnd.Wrap = Microsoft.Office.Interop.Word.WdFindWrap.wdFindContinue;
+
+                fnd.Text = pair.Key;
+                fnd.Replacement.Text = pair.Value;
+                
+                fnd.Execute(Replace: Microsoft.Office.Interop.Word.WdReplace.wdReplaceAll);
+            }
+
+            docObj.Save();
+            docObj.Close(ref missing, ref missing, ref missing);
+        }
+
+        private void DisableButtons() 
+        {
+            button1.Enabled = false;
+            buttonCSV.Enabled = false;
+            buttonDocTemplate.Enabled = false;
+            buttonTargetFolder.Enabled = false;
+            toolStripProgressBar1.Visible = true;
+        }
+
+        private void EnableButtons() 
+        {
+            button1.Enabled = true;
+            buttonCSV.Enabled = true;
+            buttonDocTemplate.Enabled = true;
+            buttonTargetFolder.Enabled = true;
+            toolStripProgressBar1.Visible = false;
+        }
+    }
+}
